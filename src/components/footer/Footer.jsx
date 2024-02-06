58059d867fa30d2a786ac8d81832f46591532549
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-02-06 18:27:59 +0530

    create footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-white flex rounded-md justify-center items-center h-14">
+      <div className="text-black">Made By Roshan ❤️ </div>
+    </div>
+  );
+};
+
+export default Footer;
